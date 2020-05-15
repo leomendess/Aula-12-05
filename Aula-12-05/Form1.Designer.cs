395f73aa932848c5c2dda7a32db1b5f9f5444c64
Leonardo <Leonardo@DESKTOP-HN6BKFT>
2020-05-15 20:12:50 -0300

    Add project files.

---
+++ Aula-12-05/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Aula_12_05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNumeros = new System.Windows.Forms.ListBox();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbNumeros
+            // 
+            this.lbNumeros.FormattingEnabled = true;
+            this.lbNumeros.Location = new System.Drawing.Point(12, 12);
+            this.lbNumeros.Name = "lbNumeros";
+            this.lbNumeros.Size = new System.Drawing.Size(120, 381);
+            this.lbNumeros.TabIndex = 1;
+            this.lbNumeros.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(182, 36);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(75, 23);
+            this.btnFor.TabIndex = 2;
+            this.btnFor.Text = "For";
+            this.btnFor.UseVisualStyleBackColor = true;
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(182, 77);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(75, 23);
+            this.btnWhile.TabIndex = 3;
+            this.btnWhile.Text = "While";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(182, 124);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(75, 23);
+            this.btnDo.TabIndex = 4;
+            this.btnDo.Text = "Do - While";
+            this.btnDo.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(182, 370);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 411);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.btnWhile);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.lbNumeros);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Teste estrutura de repetição";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbNumeros;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
